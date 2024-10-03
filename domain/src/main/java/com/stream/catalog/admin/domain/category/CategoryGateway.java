7ae66dfda1d165930e166b69052a1f073acfe4b5
emersonoliveira01 <emersongpi@hotmail.com>
2024-10-03 15:46:09 -0300

    implement interface gateway category

---
+++ domain/src/main/java/com/stream/catalog/admin/domain/category/CategoryGateway.java
@@ -0,0 +1,18 @@
+package com.stream.catalog.admin.domain.category;
+
+import com.stream.catalog.admin.domain.pagination.Pagination;
+
+import java.util.Optional;
+
+public interface CategoryGateway {
+
+    Category create(Category aCategory);
+
+    void deleteById(Category anId);
+
+    Optional<Category> findById(CategoryID anId);
+
+    Category update(Category aCategory);
+
+    Pagination<Category> findAll(CategorySearchQuery aQuery);
+}
