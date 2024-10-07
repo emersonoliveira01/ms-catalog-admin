3d525bdade87db86a7fbc9af3820f06b72eaa7db
emersonoliveira01 <emersongpi@hotmail.com>
2024-10-07 09:50:20 -0300

    implement create category usecase

---
+++ application/src/main/java/com/stream/catalog/admin/application/category/create/CreateCategoryUseCase.java
@@ -0,0 +1,7 @@
+package com.stream.catalog.admin.application.category.create;
+
+import com.stream.catalog.admin.application.UseCase;
+
+public abstract class CreateCategoryUseCase extends UseCase<CreateCategoryCommand, CreateCategoryOutput> {
+
+}
--- infrastructure/src/main/java/com/stream/catalog/admin/infrastructure/Main.java
+++ infrastructure/src/main/java/com/stream/catalog/admin/infrastructure/Main.java
@@ -5,6 +5,6 @@ import com.stream.catalog.admin.application.UseCase;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println( new UseCase().execute());
+
     }
 }
