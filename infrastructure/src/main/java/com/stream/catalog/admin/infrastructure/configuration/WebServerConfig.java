78aad92b9f115cb8141d3a99b91f32932f18e59b
emersonoliveira01 <emersongpi@hotmail.com>
2024-10-24 16:06:03 -0300

    configuration infrastructure spring boot and profiles

---
+++ infrastructure/src/main/java/com/stream/catalog/admin/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.stream.catalog.admin.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.stream.catalog.admin")
+public class WebServerConfig {
+}
