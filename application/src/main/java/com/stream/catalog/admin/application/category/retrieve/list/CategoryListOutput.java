5b625b7323148130c70fa45a99428ffcdfee2350
emersonoliveira01 <emersongpi@hotmail.com>
2024-10-24 15:02:15 -0300

    implementing the category list use case

---
+++ application/src/main/java/com/stream/catalog/admin/application/category/retrieve/list/CategoryListOutput.java
@@ -0,0 +1,26 @@
+package com.stream.catalog.admin.application.category.retrieve.list;
+
+import com.stream.catalog.admin.domain.category.Category;
+import com.stream.catalog.admin.domain.category.CategoryID;
+
+import java.time.Instant;
+
+public record CategoryListOutput(
+        CategoryID id,
+        String name,
+        String description,
+        boolean isActive,
+        Instant createdAt,
+        Instant deletedAt
+) {
+    public static CategoryListOutput from(final Category aCategory) {
+        return new CategoryListOutput(
+                aCategory.getId(),
+                aCategory.getName(),
+                aCategory.getDescription(),
+                aCategory.isActive(),
+                aCategory.getCreateAt(),
+                aCategory.getDeleteAt()
+        );
+    }
+}
---
+++ application/src/main/java/com/stream/catalog/admin/application/category/retrieve/list/DefaultListCategoriesUseCase.java
@@ -0,0 +1,22 @@
+package com.stream.catalog.admin.application.category.retrieve.list;
+
+import com.stream.catalog.admin.domain.category.CategoryGateway;
+import com.stream.catalog.admin.domain.category.CategorySearchQuery;
+import com.stream.catalog.admin.domain.pagination.Pagination;
+
+import java.util.Objects;
+
+public class DefaultListCategoriesUseCase extends ListCategoriesUseCase {
+
+    private final CategoryGateway categoryGateway;
+
+    public DefaultListCategoriesUseCase(final CategoryGateway categoryGateway) {
+        this.categoryGateway = Objects.requireNonNull(categoryGateway);
+    }
+
+    @Override
+    public Pagination<CategoryListOutput> execute(final CategorySearchQuery aQuery) {
+        return this.categoryGateway.findAll(aQuery)
+                .map(CategoryListOutput::from);
+    }
+}
